e4a67b49950eb418935baac6dab0f9751d96285c
Judit Langer <judit.langer@aon.at>
2023-09-10 21:23:47 +0200

    Implement returning index.html from directory

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Zero-dependency web server
 
 Command line program without any dependencies that returns the files of the 'Public' directory if the server is called
-If you call the server with a specific file in the Public folder, just the file will be returned
+If you call the server with a specific file in the Public folder, just the file will be returned, otherwise a 404 error message is displayed.
--- index.js
+++ index.js
@@ -11,59 +11,84 @@ try {
   console.error(err);
 }
 
-let fileList = [];
-const files = fs.readdirSync(folderPath);
+// read content of directories recursively
+function getFiles(dir, files = []) {
+  const fileList = fs.readdirSync(dir);
+  for (const file of fileList) {
+    const name = `${dir}/${file}`;
+    // Check if the current file/directory is a directory
+    if (fs.statSync(name).isDirectory()) {
+      // recursion
+      getFiles(name, files);
+      files.push('/' + file + '/');
+    } else {
+      // If it is a file, push the full path to the files array
+      if (dir !== './Public/') {
+        files.push(name.slice(9));
+      } else {
+        files.push('/' + file);
+      }
+    }
+  }
+  return files;
+}
 
-const directoryFiles = files.filter((file) =>
-  fs.statSync(folderPath + file).isDirectory(),
-);
-directoryFiles.forEach((file) => {
-  fileList = files.concat(fs.readdirSync(folderPath + file));
-  console.log(fs.readdirSync(folderPath + file));
-});
-
-console.log(directoryFiles);
-console.log(files);
-console.log(fileList);
-
-// files.forEach(function (file) {
-//   if (fs.statSync(folderPath + file).isDirectory()) {
-//     fs.readdirSync(folderPath + file, (fileInDirectory) =>
-//       fileList.push(fileInDirectory),
-//     );
-//   }
-
-//   fileList.push(file);
-// });
+const filesInTheFolder = getFiles(folderPath);
 
 // create server
 const port = 3000;
 const server = http.createServer(function (req, res) {
-  console.log(req.url);
-  console.log(req.url.slice(1));
-
-  if (fileList.includes(req.url.slice(1))) {
-    fs.readFile(folderPath + req.url.slice(1), function (error, content) {
+  console.log(req.rawHeaders);
+  // go to index.html per default
+  if (req.url === '/') {
+    fs.readFile(folderPath + 'index.html', function (error, content) {
       if (error) {
         console.log(error);
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        es.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(content);
         res.end();
       }
     });
+    // check if file exists in Public directory
+  } else if (filesInTheFolder.includes(req.url)) {
+    // check if file is a directory, if yes - check if in this directory is a index file
+    if (req.url.endsWith('/')) {
+      let fileToUse;
+      if (filesInTheFolder.includes(req.url + 'index.html')) {
+        fileToUse = req.url.slice(1) + 'index.html';
+      } else if (filesInTheFolder.includes(req.url + 'index.htm')) {
+        fileToUse = req.url.slice(1) + 'index.htm';
+      }
+      if (fileToUse) {
+        fs.readFile(folderPath + fileToUse, function (error, content) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(content);
+            res.end();
+          }
+        });
+      }
+      // use matching file from the Public directory
+    } else {
+      fs.readFile(folderPath + req.url.slice(1), function (error, content) {
+        if (error) {
+          console.log(error);
+        } else {
+          // const contentType = req.getHeader('content-type');
+          res.write(content);
+          res.end();
+        }
+      });
+    }
+    // send 404 error if the file doesn't exist
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.write('Error 404 - file not found!');
     res.end();
   }
 });
-
-// if (fileList.includes(req.url.slice(1))) {
-//   console.log(req.url);
-// } else {
-//   console.log('404');
-// }
-// });
 
 server.listen(port);
